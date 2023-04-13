0ae20da35195ef7aaad955ade67d6015d28ebedb
SisAdminNsk <artemoktan777@gmail.com>
2023-04-14 03:48:54 +0700

    added global constants file

---
+++ Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+// FILE CONTAINS DATA ABOUT ALL CONSTANTS USING IN GAME //
+const int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 800;
+const int STEP_COUNTER_XCOORD = 15, STEP_COUNTER_YCOORD = 10;// position of step counter 
+const int FONT_SIZE = 50;// font size of all using text in program
+const int TIME_MESSAGE_XCOORD = 475, TIME_MESSAGE_YCOORD = 15;// position of time message 
+const int PUZZLE_WIDTH = 160, PUZZLE_HEIGHT = PUZZLE_WIDTH;// size of pyatnashka puzzle
+const int EMPTY_PUZZLE_XCOORD = 480, EMPTY_PUZZLE_YCOORD = 640;
+////////////////////////////////////////////////////////////
