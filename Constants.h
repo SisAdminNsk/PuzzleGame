6b7a2143308f0e114a8ec1ed5d49e0388ee5612a
SisAdminNsk <artemoktan777@gmail.com>
2023-04-15 18:51:38 +0700

    update general consts file

--- Constants.h
+++ Constants.h
@@ -3,7 +3,7 @@
 const int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 800;
 const int STEP_COUNTER_XCOORD = 15, STEP_COUNTER_YCOORD = 10;// position of step counter 
 const int FONT_SIZE = 50;// font size of all using text in program
-const int TIME_MESSAGE_XCOORD = 475, TIME_MESSAGE_YCOORD = 15;// position of time message 
+const int TIME_MESSAGE_XCOORD = 450, TIME_MESSAGE_YCOORD = 15;// position of time message 
 const int PUZZLE_WIDTH = 160, PUZZLE_HEIGHT = PUZZLE_WIDTH;// size of pyatnashka puzzle
 const int EMPTY_PUZZLE_XCOORD = 480, EMPTY_PUZZLE_YCOORD = 640;
 ////////////////////////////////////////////////////////////
